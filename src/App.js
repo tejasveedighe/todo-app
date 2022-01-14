26b76f02d9905a233a11a0666c5f57efa4eedd62
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-01-14 16:41:14 +0530

    Made the whole App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Completed = ({ completedList }) => {
+  if (completedList.length < 1) {
+    return <></>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {completedList.map((query) => (
+        <div key={query.task}>
+          {" "}
+          <li>{query.task}</li>
+        </div>
+      ))}
+    </ul>
+  );
+};
+
+const Input = ({ newTodo, addTodo, handleTodoChange }) => {
+  return (
+    <form onSubmit={addTodo}>
+      <input value={newTodo} onChange={handleTodoChange}></input>
+      <button type="submit">add</button>
+    </form>
+  );
+};
+
+const TodoList = ({ todoList, handleTaskComplete }) => {
+  if (todoList.length < 1) {
+    return <><br></br>No Task</>;
+  }
+
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <div key={todoList.indexOf(todo)}>
+          <li>
+            {todo.task}{" "}
+            <button
+              onClick={() => {
+                handleTaskComplete(todo);
+              }}
+            >
+              complete
+            </button>
+          </li>
+        </div>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [todoList, settodoList] = useState([]);
+  const [completedList, setCompleteList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleTodoChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    if(!newTodo.length) {
+      alert("Cannot add empty task");
+    }
+
+    const newTask = {
+      task: newTodo,
+      isComplete: false,
+    };
+
+    settodoList(todoList.concat(newTask));
+    setNewTodo("");
+  };
+
+  const handleTaskComplete = (todo) => {
+    settodoList(todoList.filter((query) => query !== todo));
+    setCompleteList(completedList.concat(todo));
+  };
+
+  return (
+    <div>
+      <h1> Todo List </h1>
+      <Input
+        newTodo={newTodo}
+        handleTodoChange={handleTodoChange}
+        addTodo={addTodo}
+      />
+      <TodoList todoList={todoList} handleTaskComplete={handleTaskComplete} />
+      <h1>Completed Task</h1>
+      <Completed completedList={completedList} />
     </div>
   );
-}
+};
 
 export default App;
